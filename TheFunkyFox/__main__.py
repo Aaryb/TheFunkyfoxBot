97cf7994260effba61074841dd31caddefbaabfb
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-02-11 12:56:20 +0530

    Update __main__.py

--- TheFunkyFox/__main__.py
+++ TheFunkyFox/__main__.py
@@ -1,14 +1,14 @@
 import asyncio
 import importlib
 from pyrogram import idle
-from Natasha.modules import ALL_MODULES
+from TheFunkyFox.modules import ALL_MODULES
 
 loop = asyncio.get_event_loop()
 
 
 async def sumit_boot():
     for all_module in ALL_MODULES:
-        importlib.import_module("Natasha.modules." + all_module)
+        importlib.import_module("TheFunkyFox.modules." + all_module)
     print("────────────BOT START────────────")
     await idle()
     print("GoodBye! Stopping Bot")
