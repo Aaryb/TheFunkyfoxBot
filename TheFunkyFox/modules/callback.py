f54cba25b4fb60c35b33100425944196b69a5852
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-02-11 16:44:52 +0530

    Update and rename cbb.py to callback.py

--- TheFunkyFox/modules/callback.py
+++ TheFunkyFox/modules/callback.py
@@ -1,5 +1,5 @@
 from pyrogram import __version__, enums
-from bot import Bot
+from TheFunkyFox import TheFunkyFox as Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
@@ -8,12 +8,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/rakeshyt/DevilFilesStore'>Click here</a>\n○ Channel : @RYMOFFICIAL\n○ Support Group : @JAIHINDCHATTING</b>",
+            text = f"<b>○ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>ᴛʜɪs ᴘᴇʀsᴏɴ</a>\n○ ʟᴀɴɢᴜᴀɢᴇ : <code>ᴘʏᴛʜᴏɴ 3.11.1</code>\n○ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ ᴀsʏɴᴄɪᴏ {__version__}</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("🍒 ᴄʟᴏsᴇ", callback_data = "close")
                     ]
                 ]
             )
