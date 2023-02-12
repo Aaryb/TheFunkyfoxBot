596601b7aada0b8467db2ae5f1ebaeb66b8e9f77
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-02-12 11:49:18 +0530

    Update __init__.py

--- TheFunkyFox/__init__.py
+++ TheFunkyFox/__init__.py
@@ -1,15 +1,20 @@
+import sys
 import asyncio
 import logging
 import time
 import config
+import pyromod.listen
+from datetime import datetime
+from config import LOGGER, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID
 from importlib import import_module
 from os import environ, getenv, listdir, path
-#from dotenv import load_dotenv
-from pyrogram import Client
+from pyrogram import Client, enums
+from pyrogram.enums import ParseMode
+
 
 
 loop = asyncio.get_event_loop()
-#load_dotenv()
+
 boot = time.time()
 
 
@@ -27,6 +32,44 @@ TheFunkyFox = Client(
     bot_token=config.BOT_TOKEN,
 )
 
+self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        self.uptime = datetime.now()
+
+        if FORCE_SUB_CHANNEL:
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                self.invitelink = link
+            except Exception as a:
+                self.LOGGER(__name__).warning(a)
+                self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/JaiHindChatting for support")
+                sys.exit()
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER(__name__).warning(e)
+            self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/JaiHindChatting for support")
+            sys.exit()
+
+        self.set_parse_mode(ParseMode.HTML)
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/RymOfficial")
+        self.username = usr_bot_me.username
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped.")
 
 async def TheFunkyFox_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
