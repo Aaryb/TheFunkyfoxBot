20a60c0ca775d4e36a4a1701d027306908e66dee
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-02-12 15:33:22 +0530

    Update callback.py

--- TheFunkyFox/callback.py
+++ TheFunkyFox/callback.py
@@ -1,5 +1,5 @@
 from pyrogram import __version__, enums
-from TheFunkyFox import TheFunkyFox as Bot
+from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
