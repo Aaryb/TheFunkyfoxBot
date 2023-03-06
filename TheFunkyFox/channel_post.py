cdfe7491c17eed2ae9e0dff9f80477cb8e49d10b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-06 19:05:20 +0530

    Update channel_post.py

--- TheFunkyFox/channel_post.py
+++ TheFunkyFox/channel_post.py
@@ -41,7 +41,7 @@ async def new_post(client: Client, message: Message):
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ ᴄᴏᴘʏ ᴜʀ ♻️ʟ", url=f'{link}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ ᴄᴏᴘʏ ᴜʀʟ ♻️", url=f'{link}')]])
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
