d34b4edba18fd9e6c5f6f2752a0c0248eacea964
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-03-14 10:54:51 +0530

    Update link_generator.py

--- TheFunkyFox/link_generator.py
+++ TheFunkyFox/link_generator.py
@@ -37,7 +37,7 @@ async def batch(client: Client, message: Message):
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ ʜᴇʀᴇ ɪs ʏᴏᴜʀ sʜᴏʀᴛ ʟɪɴᴋ ♻️", url=await get_shortlink(f'{link}')]]))
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("♻️ ʜᴇʀᴇ ɪs ʏᴏᴜʀ sʜᴏʀᴛ ʟɪɴᴋ ♻️", url=await get_shortlink(f'{link}'))]])
     await second_message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ɴᴏʀᴍᴀʟ ʟɪɴᴋ\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
