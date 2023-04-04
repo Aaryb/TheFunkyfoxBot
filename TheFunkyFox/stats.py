84ca241881d63ec9d1b026e4f4f447274a4134e0
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-04-04 08:59:34 +0530

    Update stats.py

--- TheFunkyFox/stats.py
+++ TheFunkyFox/stats.py
@@ -6,6 +6,10 @@ from pyrogram import filters
 from config import URL_SHORTNER_API_KEY, URL_SHORTNER_API
 from TheFunkyFox.helper_func import get_readable_time
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
 USER_REPLY_TEXT = """<code> ᴀʀᴇ ʙʜᴀɪʏᴀ.. ᴀᴀᴘ ᴋᴏɴ ᴄʜʟᴇ ᴊᴀᴀᴏ ʏʜᴀ sᴇ ᴍᴜᴊʜᴇ ᴀᴀᴘsᴇ ʙᴀᴀᴛ ɴʜɪ ᴋʀɴɪ. </code>"""
 
 
