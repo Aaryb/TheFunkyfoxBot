3af550dee91edc89d5a70dce3d27dfe8475ce60c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2023-04-10 22:27:03 +0530

    Update link_generator.py

--- TheFunkyFox/link_generator.py
+++ TheFunkyFox/link_generator.py
@@ -1,7 +1,6 @@
 from bot import Bot
 from config import OWNER_ID
 from pyrogram import Client, filters, enums
-from TheFunkyFox.stats import get_shortlink
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from TheFunkyFox.helper_func import encode, get_message_id
 
@@ -37,7 +36,7 @@ async def batch(client: Client, message: Message):
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("💲 ʜᴇʀᴇ ɪs ʏᴏᴜʀ sʜᴏʀᴛɴᴇʀ ʟɪɴᴋ 💲", url=await get_shortlink(f'{link}'))]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("💲 ʜᴇʀᴇ ɪs ʏᴏᴜʀ sʜᴏʀᴛɴᴇʀ 💲", url=f'{link}')]])
     await second_message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ ɴᴏʀᴍᴀʟ ʟɪɴᴋ\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
